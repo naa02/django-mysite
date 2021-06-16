ad76129e5b354603fdcaae9cf720b1b662436a35
naa02 <lny18136@naver.com>
2021-06-16 14:14:44 +0900

    제주도, 강원도  추가

--- common/views.py
+++ common/views.py
@@ -67,8 +67,9 @@ def forest(request):
             except Exception as e:
                 continue
             firstimage = element.find('firstimage').text # 원본 대표 이미지
-   
-            data = {"title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
+            contentid = element.find('contentid').text # 컨텐트 id값
+            
+            data = {"contentid":contentid, "title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
 
             forestList.append(data)
 
@@ -119,8 +120,9 @@ def forest2(request):
                 firstimage = element.find('firstimage').text # 원본 대표 이미지
             except Exception as e:
                 continue
-   
-            data = {"title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
+            contentid = element.find('contentid').text # 컨텐트 id값
+
+            data = {"contentid":contentid, "title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
 
             forestList.append(data)
 
@@ -128,10 +130,111 @@ def forest2(request):
     print(forestList)
     return render(request,'common/forest2.html',context)
 
+def forest3(request):
+    """
+    제주도의 자연관광지 api 연동
+    """
+    serviceUrl = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList'
+    serviceKey = 'ffxl4VMnlHEnjLKdDw2uaUS%2BPXvEczJ2mROokL5mf4n4mvo%2Face3eraz6GHHH%2FlEEeWYdcpVp7JVhbEkzsS6PA%3D%3D'
+    seviceKey_decode = unquote(serviceKey)
 
+    numOfRows = '40' # 한 페이지 결과 수
+    pageNo = '1' # 페이지 번호
+    MobileOS = 'ETC' # OS 구분
+    MobileApp = 'TourAPI3.0_Guide' # 서비스명
+    listYN = 'Y' # 목록 구분
+    arrange = 'B' # 정렬 구분 (A=제목순, B=조회순, C=수정일순, D=생성일순, E=거리순)
+    contentTypeId = '12' # 관광타입(관광지)
+    areaCode = '39' # 지역코드(제주도)
+    cat1 = 'A01' # 대분류(자연)
 
+    parameters = {"serviceKey":seviceKey_decode, "numOfRows" : numOfRows, "pageNo" : pageNo,
+                    "MobileOS" : MobileOS, "MobileApp" : MobileApp, "listYN" : listYN, 
+                    "arrange" : arrange, "contentTypeId" : contentTypeId, "areaCode" : areaCode,
+                    "cat1" : cat1 }
+    response = requests.get(serviceUrl, params=parameters)
 
+    print(response.text) # XML로 가져옴
 
+    forestList=[]
+    
+    if response.status_code == 200:
+        tree = elemTree.fromstring(response.text)
+        iter = tree.iter(tag="item")
+
+        for element in iter:
+            title = element.find('title').text # 제목
+            addr1 = element.find('addr1').text # 주소
+            try:
+                addr2 = element.find('addr2').text # 상세주소
+            except Exception as e:
+                continue
+            try:
+                firstimage = element.find('firstimage').text # 원본 대표 이미지
+            except Exception as e:
+                continue
+            contentid = element.find('contentid').text # 컨텐트 id값
+
+            data = {"contentid":contentid, "title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
+
+            forestList.append(data)
+
+    context = {'forestList' : forestList}
+    print(forestList) # List에 넣어서 출력
+    return render(request,'common/forest3.html',context)
+
+def forest4(request):
+    """
+    강원도의 자연관광지 api 연동
+    """
+    serviceUrl = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList'
+    serviceKey = 'ffxl4VMnlHEnjLKdDw2uaUS%2BPXvEczJ2mROokL5mf4n4mvo%2Face3eraz6GHHH%2FlEEeWYdcpVp7JVhbEkzsS6PA%3D%3D'
+    seviceKey_decode = unquote(serviceKey)
+
+    numOfRows = '40' # 한 페이지 결과 수
+    pageNo = '1' # 페이지 번호
+    MobileOS = 'ETC' # OS 구분
+    MobileApp = 'TourAPI3.0_Guide' # 서비스명
+    listYN = 'Y' # 목록 구분
+    arrange = 'B' # 정렬 구분 (A=제목순, B=조회순, C=수정일순, D=생성일순, E=거리순)
+    contentTypeId = '12' # 관광타입(관광지)
+    areaCode = '32' # 지역코드(강원도)
+    cat1 = 'A01' # 대분류(자연)
+
+    parameters = {"serviceKey":seviceKey_decode, "numOfRows" : numOfRows, "pageNo" : pageNo,
+                    "MobileOS" : MobileOS, "MobileApp" : MobileApp, "listYN" : listYN, 
+                    "arrange" : arrange, "contentTypeId" : contentTypeId, "areaCode" : areaCode,
+                    "cat1" : cat1 }
+    response = requests.get(serviceUrl, params=parameters)
+
+    print(response.text) # XML로 가져옴
+
+    forestList=[]
+    
+    if response.status_code == 200:
+        tree = elemTree.fromstring(response.text)
+        iter = tree.iter(tag="item")
+
+        for element in iter:
+            title = element.find('title').text # 제목
+            addr1 = element.find('addr1').text # 주소
+            try:
+                addr2 = element.find('addr2').text # 상세주소
+            except Exception as e:
+                continue
+            try:
+                firstimage = element.find('firstimage').text # 원본 대표 이미지
+            except Exception as e:
+                continue
+            contentid = element.find('contentid').text # 컨텐트 id값
+
+            data = {"contentid":contentid, "title":title, "addr1":addr1, "addr2 ":addr2, "firstimage":firstimage}
+
+            forestList.append(data)
+
+    context = {'forestList' : forestList}
+    print(forestList) # List에 넣어서 출력
+    return render(request,'common/forest4.html',context)
 
 
 @login_required(login_url='common:login')
