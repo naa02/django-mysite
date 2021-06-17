017b10c6a20f0d87e867e89e110e964f010ded87
naa02 <lny18136@naver.com>
2021-06-17 00:24:21 -0700

    connect MySQL

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+	'USER':'root',
+	'PASSWORD':'!@Nn04200224',
+	'HOST':'192.168.21.144',
+	'PORT':'3306',
     }
 }
 
